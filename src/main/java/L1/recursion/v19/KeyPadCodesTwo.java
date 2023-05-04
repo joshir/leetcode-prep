ed2238b199e88ecef20166c67eb29d5185aa4ba5
joshir <hareepjoshi@gmail.com>
2023-05-04 09:46:27 -0500

    print right away  instead of storing all the results

---
+++ src/main/java/L1/recursion/v19/KeyPadCodesTwo.java
@@ -0,0 +1,48 @@
+package L1.recursion.v19;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class KeyPadCodesTwo {
+  private static final String[] phoneCodes = {
+    ".;",
+    "abc",
+    "def",
+    "ghi",
+    "jkl",
+    "mno",
+    "pqrs",
+    "tu",
+    "vqx",
+    "yz"
+  };
+  public static void main(String[] args) {
+    getKeyPadCode("678", "");
+  }
+
+  public static void getKeyPadCode(String str, String result) {
+    if (str.length() == 0) {
+      System.out.println(result);
+      return;
+    }
+
+    String start = phoneCodes[str.charAt(0)-'0'];
+    for (int index = 0; index < start.length(); index++) {
+      char codeStart = start.charAt(index);
+      getKeyPadCode(str.substring(1),result +  codeStart);
+    }
+
+    /**
+     *                                      678
+     *                       p|         q|         r|        s|
+     *                      78          78         78        78
+     *                   t|    u|    t|    u|   t|    u|   t|   u|
+     *                   8     8     8     8    8     8    8    8
+     *                  |||   |||  |||    |||  |||     ...
+     *                                      ...
+     *
+     */
+
+  }
+}
