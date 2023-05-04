8b41f13518acfb22ea2d9aec7af86352765573fc
joshir <hareepjoshi@gmail.com>
2023-05-04 12:59:21 -0500

    ver 2 with streams

--- src/main/java/L1/recursion/v22/DiagonalMazePathsTwo.java
+++ src/main/java/L1/recursion/v22/DiagonalMazePathsTwo.java
@@ -1,11 +1,10 @@
 package L1.recursion.v22;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class DiagonalMazePathsTwo {
   public static void main(String[] args) {
-    diagonalMazePaths(0,0, 2,2,"");
+    diagonalMazePaths(0,0, 2,2,""); /* implies 3x3*/
   }
 
   public static void diagonalMazePaths(int srcRow,
@@ -19,18 +18,12 @@ public class DiagonalMazePathsTwo {
       return;
     }
 
-    if (srcRow > destRow || srcCol > destCol) ;
-
-    for (int index = 1 ; index < destCol - srcCol + 1; index++ ) {
-      diagonalMazePaths(srcRow, srcCol+1, destRow, destCol, "h"+index+ssf );
-    }
-
-    for (int index = 1 ; index < destRow - srcRow + 1; index++ ) {
-      diagonalMazePaths(srcRow+1, srcCol, destRow, destCol, "v"+index+ssf );
-    }
-
-    for (int index = 1 ; index < destRow - srcRow +1 && index < destCol - srcCol + 1; index++ ) {
-      diagonalMazePaths(srcRow+1, srcCol+1, destRow, destCol, "d"+index+ssf );
-    }
+    IntStream.range(1, destCol - srcCol + 1)
+      .forEach( index -> diagonalMazePaths(srcRow, srcCol+1, destRow, destCol, "h"+index+ssf ));
+    IntStream.range(1, destRow - srcRow + 1)
+      .forEach( index -> diagonalMazePaths(srcRow+1, srcCol, destRow, destCol, "v"+index+ssf ));
+    IntStream.range(1,
+      Math.min(destRow - srcRow +1, destCol - srcCol + 1))
+      .forEach( index -> diagonalMazePaths(srcRow+1, srcCol+1, destRow, destCol, "d"+index+ssf ));
   }
 }
