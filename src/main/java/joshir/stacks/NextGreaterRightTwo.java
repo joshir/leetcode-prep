d71a71bd2f5714ff1b877d0cc0c463f73ee930d9
joshir <hareepjoshi@gmail.com>
2023-05-06 12:14:33 -0500

    ver 2

---
+++ src/main/java/joshir/stacks/NextGreaterRightTwo.java
@@ -0,0 +1,30 @@
+package joshir.stacks;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class NextGreaterRightTwo {
+  public static void main(String[] args) {
+    int arr[] = {1,6,2,4,5,9,11,22,8,10,51};
+    System.out.println(Arrays.toString(arr));
+    System.out.println(Arrays.toString( fillGreaterRight(arr)));
+  }
+  private static int[] fillGreaterRight(int [] arr) {
+    int[] ngr = new int[arr.length];
+    LinkedList<Integer> stack = new LinkedList<>();
+
+    stack.add(0);
+    int index = 0;
+    while (++index < arr.length) {
+
+      while(stack.size() > 0 && arr[index] > arr[stack.peekLast()])
+        ngr[stack.removeLast()] = arr[index];
+      stack.add(index);
+    }
+
+    while(stack.size() > 0)
+      ngr[stack.removeLast()] = -1;
+
+    return ngr;
+  }
+}
