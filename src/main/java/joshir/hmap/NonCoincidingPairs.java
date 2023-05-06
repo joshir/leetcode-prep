4445d000e54b406ef2d4465f84e0e281ae5b8345
joshir <hareepjoshi@gmail.com>
2023-05-06 09:14:24 -0500

    use streams and zip to list of pairs

--- src/main/java/joshir/hmap/NonCoincidingPairs.java
+++ src/main/java/joshir/hmap/NonCoincidingPairs.java
@@ -2,7 +2,9 @@ package joshir.hmap;
 
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 /*
 *
@@ -12,36 +14,41 @@ import java.util.Map;
 *  sparing the case when the distance is 0
 * */
 public class NonCoincidingPairs {
-  private static class Pair {
-    Integer x;
-    Integer y;
-    Pair(Integer x, Integer y) {
+  private static class Pair<T> {
+    final T x;
+    final T y;
+    Pair(T x, T y) {
       this.x = x;
       this.y = y;
     }
   }
   public static void main(String[] args) {
+    Integer []a = new Integer[] {4, 2, 4, 6, 2, 8, 4, 2};
+    Integer []b = new Integer[] {3, 3, 6, 1, 3, 3, 6, 3};
+
     /* (4,3) (2,3) (4,6) (6,1) (2,3) (8,3) (4,6) (2,3)*/
     System.out.println(
-      numNonCoincidingPairs(
-        new int[] {4, 2, 4, 6, 2, 8, 4, 2},
-        new int[] {3, 3, 6, 1, 3, 3, 6, 3}, 8)
+      numNonCoincidingPairs(zip(a,b))
     );
   }
 
-  public static int numNonCoincidingPairs(int[] xs, int[] ys, int N) {
+  public static <T> List<Pair<T>> zip(T [] a, T [] b) {
+    return IntStream.range(0,Math.min(a.length, b.length))
+      .mapToObj(index-> new Pair<>(a[index],b[index]))
+      .toList();
+  }
+
+  public static int numNonCoincidingPairs(List<Pair<Integer>> pairs) {
     Map<Integer, Integer> cxMap = new HashMap<>();
     Map<Integer, Integer> cyMap = new HashMap<>();
-    Map<Pair, Integer> coordinateMap = new HashMap<>();
+    Map<Pair<Integer>, Integer> pairFrequency = new HashMap<>();
 
-    int pairs = 0; Pair currentPair;
-    for (int index = 0; index < N; index++){
-      currentPair = new Pair(xs[index],ys[index]);
-      pairs = cxMap.getOrDefault(xs[index], 0) + cyMap.getOrDefault(ys[index],0) - 2 * coordinateMap.getOrDefault(currentPair,0);
-      cxMap.put(xs[index], cxMap.getOrDefault(xs[index], 0) + 1);
-      cyMap.put(ys[index], cyMap.getOrDefault(ys[index], 0) + 1);
-      coordinateMap.put(currentPair, coordinateMap.getOrDefault(currentPair,0) + 1);
-    }
-    return pairs;
+    return pairs.stream().reduce(0, (partialSum, pair) -> {
+      partialSum = cxMap.getOrDefault(pair.x, 0) + cyMap.getOrDefault(pair.y,0) - 2 * pairFrequency.getOrDefault(pair,0);
+      cxMap.put(pair.x, cxMap.getOrDefault(pair.x, 0) + 1);
+      cyMap.put(pair.y, cyMap.getOrDefault(pair.y, 0) + 1);
+      pairFrequency.put(pair, pairFrequency.getOrDefault(pair,0) + 1);
+      return partialSum;
+    }, Integer::sum);
   }
 }
