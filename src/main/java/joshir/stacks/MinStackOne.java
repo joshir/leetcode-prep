bb4b2c2093daf39f1b110b8e4eef025aa484e910
joshir <hareepjoshi@gmail.com>
2023-05-07 21:38:39 -0500

    remove driver class for minstack

--- src/main/java/joshir/stacks/MinStackOne.java
+++ src/main/java/joshir/stacks/MinStackOne.java
@@ -1,73 +1,57 @@
 package joshir.stacks;
 
-import java.util.Arrays;
 import java.util.EmptyStackException;
+import java.util.Iterator;
 
-public class MinStackOne {
-  private static final MinStack<Integer> stack = new MinStack<>(20);
 
-  /*
-  * Simple stack impl using a backing array
-  * */
-  private static class MinStack<T extends Comparable<? super T>> {
+/*
+* Simple stack impl using a backing array
+* */
+public class MinStackOne<T extends Comparable<? super T>>{
 
-    private final T[] backing;
+  private final T[] backing;
 
-    private final T[] minimums;
+  private final T[] minimums;
 
-    private int min = -1;
+  private int min = -1;
 
-    private int tos = -1;
+  private int tos = -1;
 
-    @SuppressWarnings("unchecked")
-    MinStack(int size) {
-      backing = (T[]) new Comparable[size];
-      minimums = (T[]) new Comparable[size];
-    }
+  @SuppressWarnings("unchecked")
+  public MinStackOne(int size) {
+    backing = (T[]) new Comparable[size];
+    minimums = (T[]) new Comparable[size];
+  }
 
-    void push(T el) {
-      if (tos>=backing.length) throw new StackOverflowError();
-      if (tos==-1) {
-        backing[++tos] = el;
-        minimums[++min] = el;
-        return;
-      }
-      else if (el.compareTo(minimums[min]) < 0)
-        minimums[++min] = el;
-
+  void push(T el) {
+    if (tos>=backing.length) throw new StackOverflowError();
+    if (tos==-1) {
       backing[++tos] = el;
+      minimums[++min] = el;
+      return;
     }
+    else if (el.compareTo(minimums[min]) < 0)
+      minimums[++min] = el;
 
-    T pop() {
-      if (tos<=-1) throw new EmptyStackException();
-      if(minimums[min].equals(backing[tos])){
-        min--;
-      }
-      return backing[tos--];
-    }
-
-    T min() {
-      if(tos==-1)
-        throw new EmptyStackException();
-      return minimums[min];
-    }
-
-    int isRemaining() {return tos;}
+    backing[++tos] = el;
   }
 
-  public static void main(String[] args) {
-    int[] nums = new int[]{31, 22, 9, 10, 11, 2 , 3, 99, 1, 39};
-    System.out.println(Arrays.toString(nums));
-    for (Integer i: nums){
-      stack.push(i);
-      System.out.print(stack.min()+", ");
+  T pop() {
+    if (tos<=-1) throw new EmptyStackException();
+    if(minimums[min].equals(backing[tos])){
+      min--;
     }
-
-    System.out.println();
-
-    while(stack.isRemaining() >0){
-      stack.pop();
-      System.out.print(stack.min()+", ");
-    }
+    return backing[tos--];
   }
+
+  T min() {
+    if(tos==-1)
+      throw new EmptyStackException();
+    return minimums[min];
+  }
+
+  int isRemaining() {return tos;}
 }
+
+
+
