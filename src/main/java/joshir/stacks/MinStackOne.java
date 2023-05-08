a49cb590034a080e9f1a4cd073fd29e1946d35c7
joshir <hareepjoshi@gmail.com>
2023-05-07 21:40:12 -0500

    refactor

--- src/main/java/joshir/stacks/MinStackOne.java
+++ src/main/java/joshir/stacks/MinStackOne.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 
 
 /*
-* Simple stack impl using a backing array
+* Simple min stack impl using a backing array
 * */
 public class MinStackOne<T extends Comparable<? super T>>{
 
@@ -23,7 +23,7 @@ public class MinStackOne<T extends Comparable<? super T>>{
     minimums = (T[]) new Comparable[size];
   }
 
-  void push(T el) {
+  public void push(T el) {
     if (tos>=backing.length) throw new StackOverflowError();
     if (tos==-1) {
       backing[++tos] = el;
@@ -36,7 +36,7 @@ public class MinStackOne<T extends Comparable<? super T>>{
     backing[++tos] = el;
   }
 
-  T pop() {
+  public T pop() {
     if (tos<=-1) throw new EmptyStackException();
     if(minimums[min].equals(backing[tos])){
       min--;
@@ -44,13 +44,13 @@ public class MinStackOne<T extends Comparable<? super T>>{
     return backing[tos--];
   }
 
-  T min() {
+  public T min() {
     if(tos==-1)
       throw new EmptyStackException();
     return minimums[min];
   }
 
-  int isRemaining() {return tos;}
+  public boolean isEmpty() {return tos==-1;}
 }
 
 
