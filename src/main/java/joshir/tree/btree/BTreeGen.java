1981187efee60d264a8cef2ab39a7a252aa81712
joshir <hareepjoshi@gmail.com>
2023-05-11 14:34:40 -0500

    gen bin tree

--- src/main/java/joshir/tree/btree/BTreeGen.java
+++ src/main/java/joshir/tree/btree/BTreeGen.java
@@ -1,16 +1,100 @@
 package joshir.tree.btree;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BTreeGen {
-  private record TNode<T extends Number>(T data, BTreeGen.TNode<T> left, BTreeGen.TNode<T> right) {
-    @Override
-      public String toString() {
-        return data.toString();
-      }
+  /*
+   *
+   *              301
+   *        30*           3002
+   *              300*
+   *                      3001
+   *    10*
+   *
+   *
+   *        40*           4002
+   *              401*
+   *                      4001
+   *
+   *      *- non-leaf vertices
+   * */
+  private static final List<Integer> tree = List.of(
+10,
+    30,
+    300,
+    3001, -1,
+    3002, -1, -1,
+    301, -1, -1,
+    40,
+    400, -1,
+    401,
+    4001, -1,
+    4002, -1, -1, -1,
+    -1
+  );
+  private static class TNode<T extends Number> {
+    /* package public by default*/
+    T data;
+
+    /* package public by default*/
+    TNode<T> left;
+
+    /* package public by default*/
+    TNode<T> right;
+
+    TNode() {
     }
 
-  private record TNodeTree<T extends Number>(TNode<T> root) {
+    TNode(T data){
+      this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+      }
+  }
+
+  private static class  TNodeTree<T extends Number>{
+    final TNode<T> root;
+    TNodeTree (TNode<T> root) {
+      this.root = root;
+    }
   }
 
   public static void main(String[] args) {
+    TNodeTree<Integer> tree = generate();
+    TNode<Integer> root = tree.root;
+  }
+
+  /*
+  * in-order tree generation from in-order
+  * list of tree nodes
+  * */
+  public static TNodeTree<Integer> generate() {
+    TNodeTree<Integer> top = null;
+    LinkedList<TNode<Integer>> stack = new LinkedList<>();
+    TNode<Integer> parent, node;
+    for(int data : tree){
+      parent = stack.peekLast();
+      if(data != -1) {
+        node = new TNode<>(data);
+
+        if(parent == null) {
+          parent = node;
+          top = new TNodeTree<>(parent);
+        }else {
+          if(parent.left == null)
+            parent.left = node;
+          else
+            parent.right = node;
+        }
+        stack.add(node);
+      }
+      else
+        stack.removeLast();
+    }
+    return top;
   }
 }
