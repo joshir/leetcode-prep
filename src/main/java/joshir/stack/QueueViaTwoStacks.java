45c154eb790e635f5eacd3ca596b9ce39ff5d2f0
joshir <hareepjoshi@gmail.com>
2023-05-15 21:15:26 -0500

    finish QueueViaStack

--- src/main/java/joshir/stack/QueueViaTwoStacks.java
+++ src/main/java/joshir/stack/QueueViaTwoStacks.java
@@ -4,20 +4,54 @@ import java.util.LinkedList;
 
 public class QueueViaTwoStacks<T> {
 
+  /*
+  * delegate to inner class
+  * */
   DoubleStackQueue<T> dsq = new DoubleStackQueue<>();
 
+  /*
+  * incoming is a stack that accepts all incoming elements and
+  * outgoing is a stack that accepts elements from the incoming
+  * stack. order of insertion is maintained overall because
+  * the first stack reverses the order and the second stack undoes the first.
+  * */
   static class DoubleStackQueue<T>{
 
-    final LinkedList<T> s1 = new LinkedList<>();
+    /* incoming stack for insertions*/
+    final LinkedList<T> incoming = new LinkedList<>();
 
-    final LinkedList<T> s2 = new LinkedList<>();
+    /* outgoing stack for extractions*/
+    final LinkedList<T> outgoing = new LinkedList<>();
 
-    void enque(T item) {}
+    void enque(T item) {
+      incoming.add(item);
+    }
 
-    T deque() {return null;}
+
+    T deque() {
+      drain();
+      return outgoing.removeLast();
+    }
+
+    /*
+     * make sure outgoing stack is empty before we add more elements to it
+     * because we don't want to disrupt the order of older elements on the
+     * outgoing stack with the new element from the incoming stack
+     * */
+    private void drain() {
+      while(outgoing.isEmpty() && !incoming.isEmpty())
+        outgoing.add(incoming.removeLast());
+    }
+
+    T peek() {
+      drain();
+      return outgoing.peekLast();
+    }
   }
 
   void enque(T item) { dsq.enque(item);}
 
   T deque() {return dsq.deque();}
+
+  T peek() {return dsq.deque();}
 }
