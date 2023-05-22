617f2da6e60b33f8fa9a48c7a2a06665cf954048
joshir <hareepjoshi@gmail.com>
2023-05-22 11:30:49 -0500

    ref

--- src/main/java/joshir/twoptr/BuyAndSellStock.java
+++ src/main/java/joshir/twoptr/BuyAndSellStock.java
@@ -2,35 +2,35 @@ package joshir.twoptr;
 
 public class BuyAndSellStock {
 
-  static class Pair {
+  static class Window {
 
     int buy;
 
     int sell;
 
-    Pair(int b, int s) {
+    Window(int b, int s) {
       this.buy = b;
       this.sell = s;
     }
   }
 
-  public static Pair maximizeProfit(int[] arr) {
-    Pair profit = null;
+  public static Window maximizeProfit(int[] arr) {
+    Window best = null;
     for(int s = 0, e = 1, curr = 0; e < arr.length; e++) {
       if(arr[s] < arr[e]){
         if(curr < arr[e] - arr[s]){
           curr= arr[e] - arr[s];
-          profit = new Pair(s,e);
+          best = new Window(s,e);
         }
       }
       else
         s = e; /* start catches up to end if arr[e] < arr[s] */
     }
-    return profit;
+    return best;
   }
 
   public static void main(String[] args) {
-    Pair p = maximizeProfit(new int[] {0,2,1,6,9,3,4,11});
+    Window p = maximizeProfit(new int[] {0,2,1,6,9,3,4,11});
     System.out.println("buy: "+ p.buy + " sell: " + p.sell);
   }
 }
