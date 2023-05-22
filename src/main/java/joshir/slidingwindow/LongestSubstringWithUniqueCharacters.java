72352253d6fecf69b652847b483284c85e594f5e
joshir <hareepjoshi@gmail.com>
2023-05-21 21:23:19 -0500

    ref and add a couple more

---
+++ src/main/java/joshir/slidingwindow/LongestSubstringWithUniqueCharacters.java
@@ -0,0 +1,31 @@
+package joshir.slidingwindow;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithUniqueCharacters {
+
+  public static String longestSubstringWithUniqueChars(String str) {
+    int s = 0, e = 0, maxlength = 0;
+    String substr = null;
+    HashMap<Character, Integer> map = new HashMap<>();
+    map.put(str.charAt(0), 0);
+
+    char curr;
+    while(e < str.length()-1) {
+      curr = str.charAt(++e);
+      if (map.containsKey(curr))
+        s = map.get(curr) + 1;
+      map.put(curr, e);
+      if(e - s + 1 > maxlength){
+        maxlength = e - s + 1;
+        substr = str.substring(s,e+1);
+      }
+    }
+    return substr;
+  }
+
+
+  public static void main(String[] args) {
+    System.out.println(longestSubstringWithUniqueChars("whatwhywhere"));
+  }
+}
--- src/main/java/joshir/slidingwindow/TargetSumSubarray.java
+++ src/main/java/joshir/slidingwindow/TargetSumSubarray.java
@@ -6,27 +6,31 @@ public class TargetSumSubarray {
 
   public int[] subarraySum(int[] arr, int target) {
     int start = 0, end = 0;
-    for (int sasum = arr[0]; start < arr.length; ){
+    int sasum = arr[0];
+    int[] retArr = null;
+    while(true) {
       if(start > end) sasum = arr[end = start];
 
       if(sasum < target) {
-        if(end == arr.length-1) break;
+        if(end==arr.length-1) break;
         sasum += arr[++end];
       }
-      else if (sasum > target)
+      else if (sasum > target){
+        if(start==arr.length) break;
         sasum -= arr[start++];
-      else if (sasum == target)
+      }
+      else{
+        retArr = new int[end-start+1];
+        for(int cursor = 0; start < end + 1;)
+          retArr[cursor++] = arr[start++];
         break;
+      }
     }
-
-    int[] retArr = new int[end-start+1];
-    for(int cursor = 0; start < end+1;)
-      retArr[cursor++] = arr[start++];
 
     return retArr;
   }
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(new TargetSumSubarray().subarraySum(new int[] {1,2,3,5,2}, 8)));
+    System.out.println(Arrays.toString(new TargetSumSubarray().subarraySum(new int[] {1,9,3,8,2}, 7)));
   }
 }
