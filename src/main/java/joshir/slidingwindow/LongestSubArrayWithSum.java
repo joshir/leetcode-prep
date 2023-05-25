fa5dacdd0be93960abbd8660fbd25925874ebe47
joshir <hareepjoshi@gmail.com>
2023-05-25 17:30:23 -0500

    sliding window easies

---
+++ src/main/java/joshir/slidingwindow/LongestSubArrayWithSum.java
@@ -0,0 +1,25 @@
+package joshir.slidingwindow;
+
+public class LongestSubArrayWithSum {
+  public static void main(String[] args) {
+    System.out.println(new LongestSubArrayWithSum().findLength(new int[]{1000, 9, 3, 8, 2}, 7));
+  }
+
+  public int findLength(int[] nums, int k) {
+    int left = 0;
+    int curr = 0;
+    int ans = 0;
+
+    for (int right = 0; right < nums.length; right++) {
+      curr += nums[right];
+      while (curr > k) {
+        curr -= nums[left];
+        left++;
+      }
+
+      ans = Math.max(ans, right - left + 1);
+    }
+
+    return ans;
+  }
+}
---
+++ src/main/java/joshir/slidingwindow/MaxSlidingWindow.java
@@ -0,0 +1,18 @@
+package joshir.slidingwindow;
+
+import java.util.LinkedList;
+
+public class MaxSlidingWindow {
+  private final LinkedList<Integer> deque = new LinkedList<>();
+
+
+  public static int[] maxSlidingWindow(int[] nums, int k) {
+
+
+    return null;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
