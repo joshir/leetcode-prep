0b2f13f7251bfa0972111f8cd5876874e4bc594d
joshir <hareepjoshi@gmail.com>
2023-05-27 14:31:55 -0500

    LongestSubstringContainingOnlyOnes

---
+++ src/main/java/joshir/slidingwindow/LongestSubstringContainingOnlyOnes.java
@@ -0,0 +1,31 @@
+package joshir.slidingwindow;
+
+public class LongestSubstringContainingOnlyOnes {
+  public int findLength(String s) {
+    int left = 0;
+    int curr = 0;
+    int ans = 0;
+
+    for (int right = 0; right < s.length(); right++) {
+      if (s.charAt(right)== '0') {
+        curr++;
+      }
+
+      while (curr > 1) {
+        if (s.charAt(left) == '0') {
+          curr--;
+        }
+
+        left++;
+      }
+
+      ans = Math.max(ans, right - left + 1);
+    }
+
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new LongestSubstringContainingOnlyOnes().findLength("1101100111"));
+  }
+}
