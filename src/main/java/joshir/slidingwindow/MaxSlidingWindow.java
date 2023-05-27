db802f87316060d472621f04eb35a15dfc5bc86f
joshir <hareepjoshi@gmail.com>
2023-05-26 21:02:54 -0500

    max element in each subarray

--- src/main/java/joshir/slidingwindow/MaxSlidingWindow.java
+++ src/main/java/joshir/slidingwindow/MaxSlidingWindow.java
@@ -1,18 +1,38 @@
 package joshir.slidingwindow;
 
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 
 public class MaxSlidingWindow {
-  private final LinkedList<Integer> deque = new LinkedList<>();
+  private ArrayDeque<Integer> deque = new ArrayDeque<>();
+  public int[] maxSlidingWindow(int[] nums, int k) {
+    int[] ans = new int[nums.length - k + 1];
 
+    for (int i = 0; i < nums.length; i++) {
+      while (!deque.isEmpty() && nums[i] > nums[deque.getLast()])
+        deque.removeLast();
 
-  public static int[] maxSlidingWindow(int[] nums, int k) {
+      /*
+      * add after making sure monotonic
+      * decreasing property is maintained
+      * */
+      deque.addLast(i);
 
+      /*
+      * when window has moved out of range
+      * remove the max element from the bottom of the deque
+      * */
+      if (deque.getFirst() + k == i)
+        deque.removeFirst();
 
-    return null;
+      if (i >= k - 1)
+        ans[i - k + 1] = nums[deque.getFirst()];
+    }
+
+    return ans;
   }
 
   public static void main(String[] args) {
-
+    System.out.println(Arrays.toString(new MaxSlidingWindow().maxSlidingWindow(new int[] {1,2,3,4,8,5,6,7}, 3)));
   }
 }
