e0d6b77a28369593e7ca5502a2ca71debdbc0973
joshir <hareepjoshi@gmail.com>
2023-05-27 16:14:15 -0500

    MinimumSizeSubarraySum

--- src/main/java/joshir/slidingwindow/MaxSubArrarAverageOne.java
+++ src/main/java/joshir/slidingwindow/MaxSubArrarAverageOne.java
@@ -1,7 +1,5 @@
 package joshir.slidingwindow;
 
-import java.util.LinkedList;
-
 public class MaxSubArrarAverageOne {
   public static void main(String[] args) {
     System.out.println(new MaxSubArrarAverageOne().findMaxAverage(new int[] {1,12,-5,-6,50,3},4));
--- src/main/java/joshir/slidingwindow/MinimumSizeSubarraySum.java
+++ src/main/java/joshir/slidingwindow/MinimumSizeSubarraySum.java
@@ -2,7 +2,16 @@ package joshir.slidingwindow;
 
 public class MinimumSizeSubarraySum {
   public int minSubArrayLen(int target, int[] nums) {
-    return 0;
+    int ans = Integer.MAX_VALUE;
+    for (int right = 0, sum = 0, left = 0; right < nums.length; right++) {
+      sum += nums[right];
+      while(sum >= target) {
+        ans = Math.min(right - left + 1, ans );
+        sum-=nums[left];
+        left++;
+      }
+    }
+    return ans;
   }
 
   public static void main(String[] args) {
