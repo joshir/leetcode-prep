94392631f676261f08f004036f853b035d775ec2
joshir <hareepjoshi@gmail.com>
2023-08-28 20:31:55 -0500

    ocd

--- src/main/java/com/joshir/ll/MiddleOfTheList.java
+++ src/main/java/com/joshir/ll/MiddleOfTheList.java
@@ -12,7 +12,7 @@ public class MiddleOfTheList {
       if(head == null || head.next == null)
         return head;
       /*
-       * fast pointer moves 2n+1 from position 1 to 3 to 5 to 7 and so on
+       * fast pointer moves 2n-1 from position 1 to 3 to 5 to 7 and so on
        * so fast pointer always overshoots end of an even sized list
        * */
       ListNode fast = head, mid = head;
