60e9d5ba24ee7b1c3162f61f5b29c1b86648e5cf
joshir <hareepjoshi@gmail.com>
2023-08-28 20:05:59 -0500

    more ll problems

---
+++ src/main/java/com/joshir/ll/RotateList.java
@@ -0,0 +1,35 @@
+package com.joshir.ll;
+
+public class RotateList {
+  class Solution {
+    private static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int val) { this.val = val; }
+    }
+    public ListNode rotateRight(ListNode head, int k) {
+      if(k == 0 || head == null || head.next == null)
+        return head;
+
+      ListNode[] m = new ListNode[501];
+      ListNode temp = head;
+      int c = 0;
+      while (temp != null) {
+        m[c++] = temp;
+        temp = temp.next;
+      }
+
+      k = k > c ? k%c: (k==c) ? 0 : k;
+      // check if k == 0 after recaliberating k
+      if(k == 0)
+        return head;
+
+      // rotate right by k
+      m[c-1].next = head;
+      head = m[c-k];
+      m[c-k-1].next = null;
+
+      return head;
+    }
+  }
+}
