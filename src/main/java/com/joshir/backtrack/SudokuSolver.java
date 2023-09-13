357d6c47243525d80034a981f144a42ad49555a6
joshir <hareepjoshi@gmail.com>
2023-09-12 21:58:34 -0500

    fun recursion

--- src/main/java/com/joshir/backtrack/SudokuSolver.java
+++ src/main/java/com/joshir/backtrack/SudokuSolver.java
@@ -1,4 +1,36 @@
 package com.joshir.backtrack;
 
 public class SudokuSolver {
+  class Solution {
+    private boolean isValidPlacement(char[][] board, int row, int col, char value) {
+      for (int index = 0; index < board.length; index++) {
+        if (board[index][col] == value ||
+          board[row][index] == value ||
+          board[3 * (row / 3) + index / 3][3 * (col / 3) + index % 3] == value) {
+          return false;
+        }
+      }
+      return true;
+    }
+    private void backtrack(char[][] board, int row, int col) {
+      if (row == board.length) // all levels done
+        return;
+      else if (col == board[0].length)
+        backtrack(board, row + 1, 0);
+      else if (board[row][col] != '.')
+        backtrack(board, row, col + 1);
+
+      for (char possible = '1'; possible <= '9'; possible++) {
+        if (isValidPlacement(board, row, col, possible)) {
+          board[row][col] = possible;
+          backtrack(board, row, col + 1);
+          board[row][col] = '.';
+        }
+      }
+    }
+
+    public void solveSudoku(char[][] board) {
+      backtrack(board, 0, 0);
+    }
+  }
 }
