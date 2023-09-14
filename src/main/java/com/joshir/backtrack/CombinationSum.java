adf5c0fc643246c58b1f7d28eeee2c1253de4535
joshir <hareepjoshi@gmail.com>
2023-09-13 21:00:25 -0500

    some more backtracking

--- src/main/java/com/joshir/backtrack/CombinationSum.java
+++ src/main/java/com/joshir/backtrack/CombinationSum.java
@@ -1,4 +1,31 @@
 package com.joshir.backtrack;
 
+import kotlin.collections.ArrayDeque;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CombinationSum {
+  class Solutions {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+      backtrack(candidates, new ArrayDeque<Integer>(), target, 0);
+      return res;
+    }
+
+    private void backtrack(int [] candidates, ArrayDeque<Integer> cur, int index, int target) {
+      if(target == 0) {
+        res.add(cur.stream().toList());
+        return;
+      }
+      if(index == candidates.length || target < 0)
+        return;
+
+      for (int i = index; i < candidates.length; i++) {
+       cur.addLast(candidates[i]);
+       backtrack(candidates, cur, index+1, target - candidates[i]);
+       cur.removeLast();
+      }
+    }
+  }
 }
