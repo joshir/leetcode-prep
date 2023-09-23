39d9c4771f821319c1d65fca36f3dd26d256a111
joshir <hareepjoshi@gmail.com>
2023-09-23 18:23:52 -0500

    backedge detection

---
+++ src/main/java/com/joshir/graph/BackedgeDetection.java
@@ -0,0 +1,47 @@
+package com.joshir.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class BackedgeDetection {
+  private int vertices;
+  private Map<Integer, List<Integer>> adj;
+  private List<List<Integer>> ll = new ArrayList<>();
+  private List<int[]> backedges = new ArrayList<>();
+  private int[] visited;
+
+  public BackedgeDetection (int v, Map<Integer, List<Integer>> adj) {
+    this.vertices = v;
+    this.adj = adj;
+    this.visited = new int[v];
+  }
+
+  public List<int[]> detect () {
+    for(int node = 0; node < vertices; node++) {
+      if(visited[node] == 0)
+        dfs(node, 0);
+    }
+    return null;
+  }
+
+  private void dfs(int node, int parent) {
+    visited[node] = 1; // visited and on the call stack
+    for (int n : adj.getOrDefault(node, new ArrayList<>())) {
+      if (visited[n] == 0) { // uncharted territory
+        dfs(n, node);
+      }
+      else if (n != parent && visited[n] == 1) { // must be a backedge since it's currently on the call stack
+        backedges.add(new int[] {node, n});
+      }
+    }
+    visited[node] = 2; // visited and off the call stack
+  }
+
+
+  public static void main(String[] args) {
+    var al = GraphUtils.adjacencyList(GraphUtils.edges, false);
+    System.out.println(Arrays.toString(new BackedgeDetection(6, al).detect().toArray()));
+  }
+}
