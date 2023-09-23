72e8ba0f82919c1a48b796b60eacac3ec24832a4
joshir <hareepjoshi@gmail.com>
2023-09-23 11:36:48 -0500

    hamiltonian paths

---
+++ src/main/java/com/joshir/graph/HamiltonianPaths.java
@@ -0,0 +1,48 @@
+package com.joshir.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class HamiltonianPaths {
+  private int vertices;
+  private Map<Integer, List<Integer>> adj;
+  private List<List<Integer>> ll = new ArrayList<>();
+  private boolean[] visited;
+  public HamiltonianPaths (int v, Map<Integer, List<Integer>> adj) {
+    this.vertices = v;
+    this.adj = adj;
+    this.visited = new boolean[v];
+  }
+
+  public List<List<Integer>> hamiltonian () {
+    for (int i = 0; i < vertices; i++){
+      if(!visited[i])
+        backtrack(i, 0, new ArrayList<>());
+    }
+    return ll;
+  }
+
+  public void backtrack(int node, int len, List<Integer> l) {
+    if (len == vertices) {
+      ll.add(l);
+      return;
+    }
+    visited[node] = true;
+    l.add(node);
+    for (int n : adj.getOrDefault(node, new ArrayList<>())) {
+      if(!visited[n])
+        backtrack(n, len + 1, l);
+    }
+    visited[node] = false;
+  }
+  public static void main(String[] args) {
+    var g = GraphUtils.adjacencyList(GraphUtils.edges, true);
+    var ll = new HamiltonianPaths(6, g).hamiltonian();
+    for (List l : ll)
+      System.out.println(Arrays.toString(l.toArray()));
+    if (ll.isEmpty())
+      System.out.println("no hamiltonian paths");
+  }
+}
