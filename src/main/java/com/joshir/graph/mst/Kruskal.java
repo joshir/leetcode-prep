c830aacf2fe5fa7a7b2a07af61877dd1167832ef
joshir <hareepjoshi@gmail.com>
2023-09-24 14:29:14 -0500

    comments

--- src/main/java/com/joshir/graph/mst/Kruskal.java
+++ src/main/java/com/joshir/graph/mst/Kruskal.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Kruskal {
+  /* vertex count*/
   private int vertices;
+  /* edge in */
   private Edge[] edges;
+  /* disjoint set for connected components */
   private UnionFind uf;
+  /* min span tree out */
   private Edge[] mst;
 
   public Kruskal (Edge[] edges, int v) {
@@ -16,13 +20,14 @@ public class Kruskal {
     this.uf = new UnionFind(vertices);
     this.mst = new Edge[vertices];
   }
+
   public Edge[] findMST() {
     // trees can have a max of v-1 vertices in order that there isn't a cycle
-    for (int i = 0, u = 0, v = 0,  edgecounter = 0; edgecounter <= vertices - 1 && i < edges.length ; i++) {
+    for (int i = 0, u = 0, v = 0,  es = 0; es <= vertices - 1 && i < edges.length ; i++) {
      if ((u = uf.find(edges[i].source)) == (v = uf.find(edges[i].destination)))
        continue;
      uf.union(u, v);
-     mst[edgecounter++] = edges[i];
+     mst[es++] = edges[i];
     }
     return mst;
   }
